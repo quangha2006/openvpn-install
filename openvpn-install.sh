bf1fb318b9bb12b8b890ce2b88ce3febc5182a20
Quang <quangskyvu@gmai.com>
2021-06-01 00:36:00 +0700

    update easy-rsa to 3.0.9

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -701,7 +701,7 @@ function installOpenVPN() {
 
 	# Install the latest version of easy-rsa from source, if not already installed.
 	if [[ ! -d /etc/openvpn/easy-rsa/ ]]; then
-		local version="3.0.7"
+		local version="3.0.9"
 		wget -O ~/easy-rsa.tgz https://github.com/OpenVPN/easy-rsa/releases/download/v${version}/EasyRSA-${version}.tgz
 		mkdir -p /etc/openvpn/easy-rsa
 		tar xzf ~/easy-rsa.tgz --strip-components=1 --directory /etc/openvpn/easy-rsa
